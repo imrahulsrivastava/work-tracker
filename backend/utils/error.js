2ae179675b650f793b268578df6f1f31c6c421b4
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-07-16 19:11:30 +0530

    formatted code

--- backend/utils/error.js
+++ backend/utils/error.js
@@ -1,13 +1,8 @@
 //globaly catching error
-function catchAsyncError (fn){
-  return function (req,res,next){
-    Promise.resolve(fn(req,res,next)).catch(next);
-  }
+function catchAsyncError(fn) {
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
 }
 
 export default catchAsyncError;
-
-
-
-
-
