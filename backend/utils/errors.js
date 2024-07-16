78fff426c929a893179f2fb40f5d947df901421e
kapil <kapil.personal9711@gmail.com>
2024-07-16 22:19:25 +0530

    handle duplicate key error

--- backend/utils/errors.js
+++ backend/utils/errors.js
@@ -1,8 +1,9 @@
 const errors = (err,req,res,next)=>{
 
+  // development mode
    if(process.env.NODE_ENV === 'development'){
-
     //checking if default err object has all things
+
     err.message = err.message || 'Internal server Error';
     err.statusCode = err.statusCode || 500;
 
@@ -15,18 +16,26 @@ const errors = (err,req,res,next)=>{
     
    }
 
-   if(process.env.NODE_ENV === 'production'){
 
+   // production errors
+   if(process.env.NODE_ENV === 'production'){
     const error = {...err};
     error.message = err.message || 'Internal server Error';
     error.statusCode = err.statusCode  || 500;
+
+  
+    //handling duplicate key
+    if(err.code === 11000){
+     const message =  `${Object.keys(err.keyValue)[0]} already exists `
+     error.message = message;
+    }
+
     res.status(error.statusCode).json({
       success:false,
       message:error.message
     });
 
    }
-
 
 }
 
