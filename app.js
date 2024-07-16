aa454428d3314911098a52271ff1a96dfa03b971
kapil <kapil.personal9711@gmail.com>
2024-07-16 15:28:14 +0530

    intial-server-setup

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hello");
+import express from 'express';
+
+const app = express();
+
+
+
+app.get('/',(req,res)=>{
+  res.send('hi from server');
+})
+
+
+const PORT  =  process.env.PORT || 8000;
+const mode = process.env.NODE_ENV || 'development';
+
+app.listen(PORT, ()=>{
+
+  console.log(`Listening on port ${PORT} in ${mode}`)
+})
+
