035bc11c26c723cb9f8c553879ee77843710893b
kapil <kapil.personal9711@gmail.com>
2024-07-16 19:10:39 +0530

    error-handler

---
+++ backend/utils/error.js
@@ -0,0 +1,13 @@
+//globaly catching error
+function catchAsyncError (fn){
+  return function (req,res,next){
+    Promise.resolve(fn(req,res,next)).catch(next);
+  }
+}
+
+export default catchAsyncError;
+
+
+
+
+
