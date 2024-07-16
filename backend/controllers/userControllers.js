bc3e9ee5ced68ccedafe5f4d40a9663e6ac01dec
kapil <kapil.personal9711@gmail.com>
2024-07-16 19:56:16 +0530

    corrected

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,11 +1,11 @@
-import captureAsyncError from '../utils/captureAsyncError'
-import usersModel from '../models/user'
+import captureAsyncError from '../utils/captureAsyncError.js'
+import usersModel from '../models/user.js'
 
 
 // register a user => /api/v1/register
 const register = captureAsyncError (async(req,res,next)=>{
   const user = await usersModel.create(req.body);
-  
+
   res.status(201).json({
     success:true,
     data:user,
